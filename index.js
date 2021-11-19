dd4c2c667d9c2929b87bbaf9563b0a9935ed084b
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-11-19 00:33:53 +0000

    commented out psuedo steps

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 Input: s = "A man, a plan, a canal: Panama"
 Output: true
 Explanation: "amanaplanacanalpanama" is a palindrome.
+
+/*
+* convert all uppercase letters to lowrcase
+* removes all non-alphanumeric characters
+    --aka remove except letters and numbers
+* alphanumeric characters include letters & numbers
+* "A man, a plan, a canal: Panama"
+    aman   nama 
+    aplan  nalp
+    acanal lanaca
+    Panama amanaP
+    namanalplanaca
+    amanaplanacanalpanama
+*/
